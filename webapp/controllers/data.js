4ef78711c9e0470b648d1fe10e96e6cee1e873ae
David Lougheed <david.lougheed@gmail.com>
2017-01-28 17:19:33 -0500

    Add ability to unzip.

--- webapp/controllers/data.js
+++ webapp/controllers/data.js
@@ -1,6 +1,7 @@
 var formidable = require('formidable');
 var path = require('path');
-var fs = require("fs");
+var fs = require('fs');
+var unzip = require('unzip2');
 
 module.exports.controller = function (objects) {
 	objects.router.get('/data', function (req, res) {
@@ -13,6 +14,7 @@ module.exports.controller = function (objects) {
 
 	objects.router.post('/data/upload', function (req, res) {
 		var form = new formidable.IncomingForm();
+		form.parse(req);
 
 		form.on('fileBegin', function (field, file) {
 			var extension = path.extname(file.name).toLowerCase();
@@ -22,24 +24,28 @@ module.exports.controller = function (objects) {
 		});
 
 		form.on('file', function(field, file) {
-			fs.rename(file.path, path.join(objects.APP_BASE_PATH, 'uploads',
-				req.user.dataValues.facebook_id + '-' + ((new Date()).getTime()).toString() + '-' + file.name),
+			var newFileName = req.user.dataValues.facebook_id + '-' + ((new Date()).getTime()).toString() + '-' + file.name;
+			fs.rename(file.path, path.join(objects.APP_BASE_PATH, 'uploads', newFileName),
 				function (err) {
 					if (err) {
 						console.error(err);
-						return res.send({ success: false, message: err });
+						return res.send({success: false, message: err});
 					}
 
-					return res.send({ success: true, message: '' });
-			});
+					// Unzip the uploaded file.
+					fs.createReadStream(path.join(objects.APP_BASE_PATH, 'uploads', newFileName)).pipe(unzip.Extract({
+						path: path.join(objects.APP_BASE_PATH, 'uploads', 'output', newFileName.slice(0, -4))
+					})).on('close', function () {
+						return res.send({success: true, message: ''});
+					});
+				}
+			);
 		});
 
 		form.on('error', function(err) {
 			console.error(err);
 			return res.send({ success: false, message: err });
 		});
-
-		form.parse(req);
 
 		// TODO: process file...
 	});
