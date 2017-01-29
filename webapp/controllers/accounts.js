9ba82c9d5df89101dca81d557363bae35abf91b2
David Lougheed <david.lougheed@gmail.com>
2017-01-28 22:46:22 -0500

    ??????

--- webapp/controllers/accounts.js
+++ webapp/controllers/accounts.js
@@ -4,50 +4,9 @@ module.exports.controller = function (objects) {
 		var callbackURL = '/auth/facebook/callback';
 
 		if (req.query.account_linking_token) {
-			objects.passport.authenticate('facebook', function (err, user, info) {
-				if (err) { return next(err); }
-				if (!user) {
-					if (req.query.redirect_uri) {
-						// Failure + auth from messenger
-						return res.redirect(req.query.redirect_uri);
-					} else {
-						return res.redirect('/');
-					}
-				}
-
-				req.logIn(user, function(err) {
-					if (err) { return next(err); }
-
-					// Success!
-
-					if (req.query.redirect_uri) {
-						request({
-							uri: 'https://graph.facebook.com/v2.8/me',
-							qs: {
-								access_token: objects.appConfig['FACEBOOK_PAGE_ACCESS_TOKEN'],
-								fields: 'recipient',
-								account_linking_token: req.query.account_linking_token
-							},
-							method: 'POST'
-						}, function (err, response, body) {
-							if (err || response.statusCode !== 200) {
-								console.error(response);
-								console.error(err);
-							}
-
-							req.user.messenger_id = body.recipient;
-							req.user.save();
-
-							return res.redirect(req.query.redirect_uri + '&authorization_code=y');
-						});
-					} else {
-						return res.redirect('/data');
-					}
-				});
-			});
-		} else {
-			objects.passport.authenticate('facebook', { callbackURL: callbackURL })(req, res, next);
+			callbackURL += '?redirect_uri=' + req.query.redirect_uri;
 		}
+		objects.passport.authenticate('facebook', { callbackURL: callbackURL })(req, res, next);
 	});
 	objects.router.get('/auth/facebook/callback', objects.passport.authenticate('facebook', { successRedirect: '/data', failureRedirect: '/' }));
 
