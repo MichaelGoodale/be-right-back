b56109655cbf8622f55173da78ff481aca2f8cc3
David Lougheed <david.lougheed@gmail.com>
2017-01-28 22:40:49 -0500

    ??????

--- webapp/controllers/accounts.js
+++ webapp/controllers/accounts.js
@@ -4,54 +4,52 @@ module.exports.controller = function (objects) {
 		var callbackURL = '/auth/facebook/callback';
 
 		if (req.query.account_linking_token) {
-			callbackURL += '?account_linking_token=' + req.query.account_linking_token + '&redirect_uri=' + req.query.redirect_uri;
-		}
-
-		objects.passport.authenticate('facebook', { callbackURL: callbackURL })(req, res, next);
-	});
-	objects.router.get('/auth/facebook/callback', function (req, res, next) {
-		objects.passport.authenticate('facebook', function (err, user, info) {
-			if (err) { return next(err); }
-			if (!user) {
-				if (req.query.redirect_uri) {
-					// Failure + auth from messenger
-					return res.redirect(req.query.redirect_uri);
-				} else {
-					return res.redirect('/');
-				}
-			}
-
-			req.logIn(user, function(err) {
+			objects.passport.authenticate('facebook', function (err, user, info) {
 				if (err) { return next(err); }
-
-				// Success!
-
-				if (req.query.redirect_uri) {
-					request({
-						uri: 'https://graph.facebook.com/v2.8/me',
-						qs: {
-							access_token: objects.appConfig['FACEBOOK_PAGE_ACCESS_TOKEN'],
-							fields: 'recipient',
-							account_linking_token: req.query.account_linking_token
-						},
-						method: 'POST'
-					}, function (err, response, body) {
-						if (err || response.statusCode !== 200) {
-							console.error(response);
-							console.error(err);
-						}
-
-						req.user.messenger_id = body.recipient;
-						req.user.save();
-
-						return res.redirect(req.query.redirect_uri + '&authorization_code=y');
-					});
-				} else {
-					return res.redirect('/data');
+				if (!user) {
+					if (req.query.redirect_uri) {
+						// Failure + auth from messenger
+						return res.redirect(req.query.redirect_uri);
+					} else {
+						return res.redirect('/');
+					}
 				}
+
+				req.logIn(user, function(err) {
+					if (err) { return next(err); }
+
+					// Success!
+
+					if (req.query.redirect_uri) {
+						request({
+							uri: 'https://graph.facebook.com/v2.8/me',
+							qs: {
+								access_token: objects.appConfig['FACEBOOK_PAGE_ACCESS_TOKEN'],
+								fields: 'recipient',
+								account_linking_token: req.query.account_linking_token
+							},
+							method: 'POST'
+						}, function (err, response, body) {
+							if (err || response.statusCode !== 200) {
+								console.error(response);
+								console.error(err);
+							}
+
+							req.user.messenger_id = body.recipient;
+							req.user.save();
+
+							return res.redirect(req.query.redirect_uri + '&authorization_code=y');
+						});
+					} else {
+						return res.redirect('/data');
+					}
+				});
 			});
-		});
+		} else {
+			objects.passport.authenticate('facebook', { callbackURL: callbackURL })(req, res, next);
+		}
 	});
+	objects.router.get('/auth/facebook/callback', objects.passport.authenticate('facebook', { successRedirect: '/data', failureRedirect: '/' }));
 
 	objects.router.get('/auth/messenger', function (req, res) {
 		if (req.isAuthenticated()) {
