76466c55b7d21929dd87371fb945aa21f8922b08
Michael Goodale <michaelericgoodale@gmail.com>
2017-01-28 20:32:26 -0500

    made model oop, added preliminary batcher

--- model.py
+++ model.py
@@ -5,76 +5,79 @@ import model_utils
 SIZE = 512
 VOCABULARY_SIZE = model_utils.VOCAB_SIZE
 BATCH_SIZE = 50
-BUCKETS =  [(5,10),(20,30),(40,50)]
+BUCKETS = model_utils.BUCKETS 
 NUMBER_OF_LAYERS = 2
 NUMBER_OF_SAMPLES = 512 
+class ConversationModel(object):
+	def __init__(self, training):
+		def get_cell():
+			return tf.nn.rnn_cell.LSTMCell(SIZE)
 
-def get_cell():
-	return tf.nn.rnn_cell.LSTMCell(SIZE)
+		self.multicell = tf.nn.rnn_cell.MultiRNNCell([get_cell()]* NUMBER_OF_LAYERS)
 
-multi_cell = tf.nn.rnn_cell.MultiRNNCell([get_cell()]* NUMBER_OF_LAYERS)
+		self.encoder_inputs = []
+		self.decoder_inputs = []
+		self.target_weights = []
 
-encoder_inputs = []
-decoder_inputs = []
-target_weights = []
+		for i in range(BUCKETS[-1][0]):
+			self.encoder_inputs.append(tf.placeholder(tf.int64, shape=[None],name="encoder{}".format(i)))
+		for i in range(BUCKETS[-1][1]+1):
+			self.decoder_inputs.append(tf.placeholder(tf.int64, shape=[None], name="decoder{}".format(i)))
+			self.target_weights.append(tf.placeholder(tf.float32, shape=[None], name="tar_weight{}".format(i)))
+		self.targets = [self.decoder_inputs[i+1] for i in range(BUCKETS[-1][1])]
 
-for i in range(BUCKETS[-1][0]):
-	encoder_inputs.append(tf.placeholder(tf.int64, shape=[None],name="encoder{}".format(i)))
-for i in range(BUCKETS[-1][1]+1):
-	decoder_inputs.append(tf.placeholder(tf.int64, shape=[None], name="decoder{}".format(i)))
-	target_weights.append(tf.placeholder(tf.float32, shape=[None], name="tar_weight{}".format(i)))
-targets = [decoder_inputs[i+1] for i in range(BUCKETS[-1][1])]
+		w = tf.get_variable("project_weights", [SIZE, VOCABULARY_SIZE])
+		w_t = tf.transpose(w)
+		b =  tf.get_variable("project_biases", [VOCABULARY_SIZE])
+		output_projection = (w, b)
 
-w = tf.get_variable("project_weights", [SIZE, VOCABULARY_SIZE])
-w_t = tf.transpose(w)
-b =  tf.get_variable("project_biases", [VOCABULARY_SIZE])
-output_projection = (w, b)
-
-def sampled_loss(inputs, labels):
-	inputs = tf.cast(inputs, tf.float32)
-	labels = tf.cast(tf.reshape(labels, [-1, 1]), tf.float32)
-	return tf.nn.sampled_softmax_loss(w_t, b, inputs, labels, NUMBER_OF_SAMPLES, VOCABULARY_SIZE)
-
-
-def seq2seq_function(encoder_inputs, decoder_inputs, feed_previous):
-	return tf.nn.seq2seq.embedding_attention_seq2seq(encoder_inputs, decoder_inputs, multi_cell, 
-		num_encoder_symbols=VOCABULARY_SIZE, 
-		num_decoder_symbols=VOCABULARY_SIZE,
-		embedding_size=SIZE, 
-		feed_previous=feed_previous,
-		output_projection=output_projection,
-		dtype=tf.float32)
-
-def seq2seq_buckets(forward_pass):
-	outputs, states = tf.nn.seq2seq.model_with_buckets(encoder_inputs, decoder_inputs, targets, target_weights, BUCKETS, lambda x, y: seq2seq_function(x, y, forward_pass), softmax_loss_function=sampled_loss)
-	if forward_pass:
-		for bucket in range(len(BUCKETS)):
-			for i, output in enumerate(outputs[bucket]):
-				outputs[bucket][i] = tf.matmul(output, output_projection[0])+output_projection[1] ##Multiplies output by weight and biases of projection
-	return outputs, states
-outputs, states = seq2seq_buckets(True)
-
-train_ops = []
-for bucket in range(len(BUCKETS)):
-	train_ops.append(tf.train.AdamOptimizer(1e-4).minimize(states[bucket]))
-saver = tf.train.Saver()
+		def sampled_loss(inputs, labels):
+			inputs = tf.cast(inputs, tf.float32)
+			labels = tf.cast(tf.reshape(labels, [-1, 1]), tf.float32)
+		return tf.nn.sampled_softmax_loss(w_t, b, inputs, labels, NUMBER_OF_SAMPLES, VOCABULARY_SIZE)
 
 
-feed_dict = {}
-for i in range(5):
-	feed_dict[encoder_inputs[i].name] = np.array([i])
-for i in range(11):	
-	feed_dict[decoder_inputs[i].name] = np.array([i])
-	feed_dict[target_weights[i].name] = np.array([i])
+		def seq2seq_function(encoder_inputs, decoder_inputs, feed_previous):
+			return tf.nn.seq2seq.embedding_attention_seq2seq(encoder_inputs, decoder_inputs, self.multicell, 
+				num_encoder_symbols=VOCABULARY_SIZE, 
+				num_decoder_symbols=VOCABULARY_SIZE,
+				embedding_size=SIZE, 
+				feed_previous=feed_previous,
+				output_projection=output_projection,
+				dtype=tf.float32)
 
+		def seq2seq_buckets(forward_pass):
+			outputs, states = tf.nn.seq2seq.model_with_buckets(encoder_inputs, decoder_inputs, targets, target_weights, BUCKETS, lambda x, y: seq2seq_function(x, y, forward_pass), softmax_loss_function=sampled_loss)
+			if forward_pass:
+				for bucket in range(len(BUCKETS)):
+					for i, output in enumerate(outputs[bucket]):
+						outputs[bucket][i] = tf.matmul(output, output_projection[0])+output_projection[1] ##Multiplies output by weight and biases of projection
+			return outputs, states
+		if training:
+			self.outputs, self.losses = seq2seq_buckets(True)
+			self.train_ops = []
+			for bucket in range(len(BUCKETS)):
+				self.train_ops.append(tf.train.AdamOptimizer(1e-4).minimize(states[bucket]))
+		else:
+			self.outputs, self.losses = seq2seq_buckets(False)
+			
+		self.saver = tf.train.Saver()
+
+	def step(self, i, session):
+		encoder_inputs, decoder_inputs, target_weights, bucket_id = model_utils.get_batch(i)
+		feed_dict = {}
+		for i in range(BUCKETS[bucket_id][0]):
+			feed_dict[encoder_inputs[i].name] = np.array([i])
+		for i in range(BUCKETS[bucket_id][1]):	
+			feed_dict[decoder_inputs[i].name] = np.array([i])
+			feed_dict[target_weights[i].name] = np.array([i])
+		feed_dict[decoder_inputs[BUCKET[bucket_id][1]+1]] = model_utils.EOS
+		session.run(self.train_ops[bucket_id], feed_dict=feed_dict)
+
+model = Conversation_Model(True)
 sess=tf.Session()
-
 sess.run(tf.global_variables_initializer())
-sess.run(train_ops[0], feed_dict=feed_dict)
-for i in range(100):
-	sess.run(train_ops[0], feed_dict=feed_dict)
-	print(i)
-output = sess.run(outputs[0], feed_dict=feed_dict)
+for i in range(10):
+	model.step(i, sess)
 sentence = [int(np.argmax(i, axis=1)) for i in output]
 print(sentence)
-save_path = saver.save(sess, "model.ckpt")
--- model_utils.py
+++ model_utils.py
@@ -1,28 +1,50 @@
 import pandas as pd
 import nltk
+import sqlite3
+import string
+import numpy as np
 #SPECIAL VOCABULARY
-
-PAD = 0
-EOS = 1
-UNK = 2
+BUCKETS = [(5,10),(20,30),(40,50)]
+GO = np.array[-1]
+PAD = np.array([0])
+EOS = np.array([1])
+UNK = np.array([2])
 VOCAB_SIZE = 4000+3 #SIZE of GloVe Corpus and special vocab
+conn = sqlite3.connect("f2db_out.db")
+c = conn.cursor()
+
 
 def tokenize_sentence(sentence):
-	tokens = nltk.word_tokenize(sentence)
-	cur = get_db().cursor()
-	sentence = in_sentence.lower().strip()
+	sentence = sentence.lower().strip().translate(dict.fromkeys(map(ord, string.punctuation)))
 	word_arr = nltk.word_tokenize(sentence)
 	sentence = []
+	print(word_arr)
 	for i, word in enumerate(word_arr):
-		word_string = (word,)
-		cur.execute("SELECT * FROM glove WHERE field1=?", word_string)
-		row = cur.fetchone()
-		if row is None:
-			sentence.append(_UNK)
+		row = c.execute("SELECT id FROM word_list WHERE word=?", (word, ))
+		word_id = row.fetchone()
+		if word_id is None:
+			sentence.append(UNK)
 		else:
-			sentence.append(np.array(row[1:]).astype(np.float32))
-
-def get_training_batch(VECTOR_SIZE, BATCH_SIZE):
+			sentence.append(np.array(word_id[0]).astype(np.float32))
+	print(sentence)
+def get_training_batch(i):
+	encode="this is an intro sentence"
+	decode="thise is an outro sentence"
+	encode_inputs = tokenize_sentence(encode)
+	decode_inputs = tokenize_sentence(decode)
+	lengths = (len(encode_input), len(decode_inputs))
+	bucket_id = 0
+	for i in range(len(BUCKETS)):
+		if lengths < BUCKETS[i]:
+			break
+		else 
+			bucket_id = i	
+	if lengths > bucket_id:
+		#TODO make clamp long sentences to max bucket
+	en_pad = [PAD]*(lengths[0]-BUCKETS[bucket_id][0])
+	encode_input = reversed(encode_inputs+en_pad)
+	de_pad = [PAD]*(lengths[1]-BUCKETS[bucket_id][1]-1)
+	decode_input = [GO]+decode_inputs+de_pad
 	
-	return np.zeros
 
+		
