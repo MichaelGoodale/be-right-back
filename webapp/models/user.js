bac0f8b4d1e9fe18f2d159a7ef6271cb60eb500f
David Lougheed <david.lougheed@gmail.com>
2017-01-28 20:50:08 -0500

    Fix messenger ID data type.

--- webapp/models/user.js
+++ webapp/models/user.js
@@ -3,7 +3,7 @@
 module.exports = function (sequelize, Sequelize) {
 	return sequelize.define('User', {
 		facebook_id: {type: Sequelize.STRING, unique: true, allowNull: false},
-		messenger_id: {type: Sequelize.String, uniquie: true},
+		messenger_id: {type: Sequelize.STRING, uniquie: true},
 		display_name: {type: Sequelize.STRING, allowNull: false}
 	});
 };
